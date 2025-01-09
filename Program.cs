0cfeffb11a7947f552e375e50ee0d9e82d5ec745
RECEP UNCU <recep.uncu@demirorenteknoloji.com>
2025-01-09 15:32:30 +0300

    Updated .gitignore to include .NET Core, VS, and NuGet rules. Added NodeVersionSwitcher project and solution files. Included nodejs-mini.png and nodejs.ico as content files. Implemented NodeVersionSwitcherContext for tray menu. Added Program class with Main method as entry point.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace NodeVersionSwitcher;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new NodeVersionSwitcherContext());
+    }
+}
